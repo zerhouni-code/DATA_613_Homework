46f512ebb08dc292a78734fa500aa63407d4ced6
Zakaria Z <zakz@mac.american.edu>
2026-09-15 14:17:20 -0400

    Solve problem 2

--- homework_1/homework_1_zakaria_zerhouni.R
+++ homework_1/homework_1_zakaria_zerhouni.R
@@ -5,3 +5,11 @@ scores <- c(72,85,91,68,84)
 scores.mean <- mean(scores)
 
 print(scores.mean)
+
+scores.median <- median(scores)
+
+scores.standard_deviation <- sd(scores)
+
+print(scores.median)
+
+print(scores.standard_deviation)
